23bb5684113dc5d71ea232b11adfac36269070ec
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-08-24 00:14:31 -0300

    feat: adjust opacity and add zinc gradient

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -13,7 +13,7 @@ const randomDelay = () => Math.random() * 5;
 export const HeroSection = () => {
   return (
     <main>
-      <div className="absolute inset-0 bg-gradient-to-r from-green-600/20 to-blue-600/20 opacity-10" />
+      <div className="absolute inset-0 bg-gradient-to-r from-green-600/20 to-blue-600/20 opacity-50" />
 
       <div className="absolute inset-0 overflow-hidden">
         {horizontalLines.map((i) => (
@@ -40,10 +40,11 @@ export const HeroSection = () => {
           />
         ))}
       </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-zinc-900" />
 
       <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
         <div className="relative z-10 max-w-4xl w-full mx-4">
-          <div className="bg-black/80 backdrop-blur-lg rounded-lg border border-gray-800 p-6 animate-fadeIn">
+          <div className="bg-black/50 backdrop-blur-lg rounded-lg border border-gray-800 p-10 animate-fadeIn">
             <h1 className="text-4xl md:text-5xl font-bold mt-2 mb-4 font-inter">Luiz Henrique Ferreira</h1>
             <p className="text-gray-400 mb-3 sm:text-xl font-inter">
               Full Stack Developer
