dad3a982dc1369512fb376fd9cbdcadbe6c269cc
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-08-28 21:45:56 -0300

    feat: bg cards gradient

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,9 +3,9 @@ export const About = () => {
     <section className="py-10 px-4 bg-zinc-900">
       <div className="max-w-4xl mx-auto">
         <div
-          className="relative p-8 rounded-2xl overflow-hidden backdrop-blur-sm border border-gray-800 animate-fadeIn-scroll"
+          className="relative p-8 rounded-2xl bg-gradient-to-r from-green-500/10 to-blue-500/10 backdrop-blur-sm border border-gray-800  font-inter"
         >
-          <div className="absolute inset-0 bg-gradient-to-r from-green-500/5 to-blue-500/5" />
+          {/* <div className="absolute inset-0 bg-gradient-to-r from-green-500/10 to-blue-500/10" /> */}
           <h2
             className="text-3xl font-bold mb-4 text-center">
             Um pouco sobre mim
--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -25,31 +25,31 @@ const experiences = [
 
 export const Experiences = () => {
   return (
-    <section className="pt-10 pb-20 px-4 bg-zinc-900 relative">
+    <section className="pt-10 pb-10 md:pb-20 px-4 bg-zinc-900 relative">
       <div className="max-w-4xl mx-auto">
-        <h2 className="text-4xl font-bold mb-12 text-center">
+        <h2 className="text-4xl font-bold mb-12 text-center font-inter">
           Experiências
         </h2>
-
-        <div className="relative z-10 space-y-10">
+        <div className="relative rounded-2xl"        >
           {experiences.map((exp, index) => (
-            <div key={index} className="mb-10 last:mb-0 group">
-              <div className="relative bg-black/50 backdrop-blur-lg rounded-lg border border-gray-800 shadow-md transition-all duration-300 group-hover:scale-[1.02] group-hover:shadow-green-500/10 py-6 pr-6 pl-14 md:pl-16">
+            <div key={index} className="mb-10 last:mb-0 relative rounded-2xl py-6 pr-6 pl-14 md:pl-16 backdrop-blur-lg border border-gray-800 shadow-md transition-all duration-300 hover:scale-[1.02] hover:shadow-white/10">
 
+              <div className="absolute inset-0 bg-gradient-to-r from-green-500/10 to-blue-500/10 rounded-2xl" />
+              <div className="z-10">
                 <div className="absolute left-6 top-6 bottom-6 w-5 flex justify-center">
 
-                  <div className="w-0.5 h-full bg-gray-700/50"></div>
+                  <div className="w-0.5 h-full bg-gray-700/80"></div>
 
-                  <div className="absolute top-0 flex-shrink-0 w-5 h-5 rounded-full bg-zinc-950 border-2 border-green-500 flex items-center justify-center z-10">
+                  <div className="absolute top-0 flex-shrink-0 w-5 h-5 rounded-full bg-green-900 flex items-center justify-center z-10">
                   </div>
                 </div>
 
-                <div>
+                <div className="relative z-10">
                   <h3 className="text-2xl text-white mb-1 font-semibold">
                     {exp.title}
                   </h3>
-                  <p className="text-gray-300 mb-4 font-semibold">
-                    {exp.company} <span className="text-gray-500 font-normal">- {exp.period}</span>
+                  <p className="text-green-500 mb-4 font-semibold">
+                    {exp.company} <span className="text-gray-300 font-normal">- {exp.period}</span>
                   </p>
 
                   <ul className="list-disc list-inside space-y-2 text-gray-300">
@@ -69,4 +69,3 @@ export const Experiences = () => {
     </section>
   )
 }
-
